b8f6872bbd25f43404fc5f2d2bbe0edbec15aab2
jhern112 <162080034+jhern112@users.noreply.github.com>
2024-04-15 14:07:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({
+  extended: true}));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
 });
 
 app.post('/submit-form', (req, res) => {
