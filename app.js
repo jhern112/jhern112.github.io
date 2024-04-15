6095f50fc17ba524ebcd16ac843a4f07162bc918
jhern112 <162080034+jhern112@users.noreply.github.com>
2024-04-15 15:53:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({
-  extended: true}));
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/submit-form', (req, res) => {
-  const username = req.body.username;
+    const username = req.body.username;
 
-  res.send(`Username is $jhern112`)
+    res.send(`Username is $jhern112`)
 });
 
-app.listen(port, ()=>{
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
